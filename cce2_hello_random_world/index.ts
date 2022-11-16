06f81367319973d43314dabe7e6b252d10c3d7ee
djvanhelmond <djvanhelmond@gmail.com>
2022-11-16 07:42:41 +0000

    job 2 done

--- cce2_hello_random_world/index.ts
+++ cce2_hello_random_world/index.ts
@@ -1,6 +1,12 @@
 export default {
 	fetch() {
-		return new Response('Hello worker!', {
+		const lookupTable = {
+			0: 'Response 0',
+			1: 'Response 1',
+			2: 'Response 2',
+			3: 'Response 3',
+		  };		  
+		return new Response(lookupTable[Math.floor(Math.random() * Object.keys(lookupTable).length)], {
 			headers: {
 				'content-type': 'text/plain',
 			},
