b4b8d6485e377d583f8555fa9a964e740cab70e3
djvanhelmond <djvanhelmond@gmail.com>
2022-11-16 11:42:18 +0000

    finished?

--- cce2_hello_random_world/index.ts
+++ cce2_hello_random_world/index.ts
@@ -1,15 +1,13 @@
 export default {
 	fetch() {
 		const lookupTable = {
-			0: 'Response 0',
-			1: 'Response 1',
-			2: 'Response 2',
-			3: 'Response 3',
-		  };		  
-		return new Response(lookupTable[Math.floor(Math.random() * Object.keys(lookupTable).length)], {
-			headers: {
-				'content-type': 'text/plain',
-			},
-		});
-	},
-};
+			0: 'https://www.os3.nl',
+			1: 'https://www.delaat.net',
+			2: 'https://github.com',
+			3: 'https://nu.nl',
+		  };
+		  const id = Math.floor(Math.random() * Object.keys(lookupTable).length) // randomly select an URL
+		  const url = new URL(lookupTable[id])
+		  return fetch(url.toString())
+		}
+	}
